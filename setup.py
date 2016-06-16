72b417b9d0199ca4ed75b462205140acb1eb7508
Murray Wham <murray.wham@ed.ac.uk>
2016-06-16 14:21:22 +0100

    Fixing requirement versions in setup.py

--- setup.py
+++ setup.py
@@ -2,16 +2,32 @@ from distutils.core import setup
 from os.path import join, abspath, dirname
 requirements = join(abspath(dirname(__file__)), 'requirements.txt')
 
+
+ops = ('<=', '>=', '==', '<', '>', '!=')
+
+
+def _translate_req(r):
+    # this>=0.3.2 -> this(>=0.3.2)
+    version = None
+    for o in ops:
+        if o in r:
+            r, version = r.split(o)
+            version = o + version
+
+    req = r
+    if version:
+        req += '(%s)' % version
+    return req
+
+
 setup(
     name='EGCG-Core',
     version='0.1',
     packages=['egcg_core', 'egcg_core.executor', 'egcg_core.executor.script_writers'],
     url='http://genomics.ed.ac.uk',
     license='',
-    author='mwhamgenomics',
-    author_email='',
     description='Common modules for use across EGCG projects. Includes logging, configuration, common '
                 'exceptions, random utility functions, and modules for interfacing with external data '
                 'sources such as EGCG\'s reporting app and Clarity LIMS instance',
-    requires=[l for l in open(requirements).readlines() if l and not l.startswith('#')]
+    requires=[_translate_req(l.strip()) for l in open(requirements) if l and not l.startswith('#')]
 )
