54d16e5727e5c294471245f5ace097b79a7eaaf4
tcezard <timothee.cezard@ed.ac.uk>
2017-02-13 13:14:57 +0000

    add manifest and add info to setup.py

--- setup.py
+++ setup.py
@@ -30,6 +30,19 @@ setup(
     long_description='Common modules for use across EGCG projects. Includes logging, configuration, common '
                      'exceptions, random utility functions, and modules for interfacing with external data '
                      'sources such as EGCG\'s reporting app and Clarity LIMS instance',
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Healthcare Industry",
+        "Intended Audience :: Science/Research",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language:: Python:: 3.3",
+        "Programming Language:: Python:: 3.4",
+        "Programming Language:: Python:: 3.5"
+    ],
+    keywords='EdingburghGenomics executor notification logging api rest',
     requires=[_translate_req(r) for r in requirements],  # metadata
-    install_requires=requirements  # actual module requirements
+    install_requires=requirements,  # actual module requirements
+    zip_safe=False
 )
